e3f8d3c0bec037d917cb89d31e043c7490526cd1
m_wallet_app <mwallet.apps@gmail.com>
2022-10-19 13:24:17 +0200

    used created DaggerCoffeeComponent to getCoffee()

--- app/src/main/java/mo/atef/codingwithnerds/codingwithnerds/MainActivity.java
+++ app/src/main/java/mo/atef/codingwithnerds/codingwithnerds/MainActivity.java
@@ -11,11 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Farm farm=new Farm();
-        River river=new River();
-
-        // manual Dependency Injection
-        Coffee coffee=new Coffee(farm, river);
+        CoffeeComponent coffeeComponent =DaggerCoffeeComponent.create();
+        coffeeComponent.getCoffee();
 
 
 
