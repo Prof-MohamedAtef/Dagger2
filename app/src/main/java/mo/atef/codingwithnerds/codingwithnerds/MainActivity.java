9a779eac512810ab22dded3fe2469f7b4f954edd
m_wallet_app <mwallet.apps@gmail.com>
2022-10-20 10:56:50 +0200

    removed everything

--- app/src/main/java/mo/atef/codingwithnerds/codingwithnerds/MainActivity.java
+++ app/src/main/java/mo/atef/codingwithnerds/codingwithnerds/MainActivity.java
@@ -9,18 +9,11 @@ import javax.inject.Inject;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Inject
-    Coffee coffee;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CoffeeComponent coffeeComponent =DaggerCoffeeComponent.builder().coffeeModule(new CoffeeModule(4)).build();
-        coffeeComponent.inject(this);
-
-        Log.v("onCreate: AtefLogs:",coffee.getCoffeeCup());
 
     }
 }
